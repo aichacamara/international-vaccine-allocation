7209a3b17d953a64dd558250711d546dc76770a6
mhveatch <84858051+mhveatch@users.noreply.github.com>
2022-07-18 16:43:37 -0400

    Add files via upload

--- gurobi-documentation/LP_indexing.py
+++ gurobi-documentation/LP_indexing.py
@@ -4,6 +4,8 @@ import gurobipy as gp
 from gurobipy import GRB
 import numpy as np
 import pandas as pd
+import math
+import time
 
 ### Numerical indexing: size m x n, with ranges [0,m-1] x [0,n-1]. max c^Ty s.t. Ay <= b y >= 0
 
@@ -11,44 +13,47 @@ M = gp.Model("Numerical Indexing")
 
 m = 2 # rows
 n = 3 # columns
-A = np.array([[1, 2, 3], # A is a matrix, not a list of lists
-    [1, 1, 0]])
 
-b = [4, 1] # b and c are lists, not row or column vectors
+# Using np arrays
+A = np.array([[1, 2, 3], # A is a matrix, refered to as A[0,0], etc.
+    [1, 1, 0]])
+b = [4, 1] # b and c are lists
 c = [1, 1, 2]
 
 # For sparse A
-# A = {i,j: 0 for i in range(m) for j in range(n)} # Like a loop, sets A = 0. Not working
+A = np.zeros((m, n))
 A[0,0] = 1
 A[0,1] = 2
 A[0,2] = 3
 A[1,0] = 1
 A[1,1] = 1
 
+# See matrix1.py, which uses sparse matrix statements from scipy.sparse 
+
+# Using lists of lists (base python) 
+# A = [[1, 2, 3], # A is a list of lists. Refer to as A[0][0] in addConstrs
+#     [1, 1, 0]]
+#
+# For sparse A
+# A = {(i,j): 0 for i in range(m) for j in range(n)} # Like a loop, sets A = 0
+# A[0][0] = 1 #etc.
+
 # To assign A from other data, say D
-# A = {{i,j: 7*D[i,j] for i in range(m-1) for j in range(n-1)}}
+# A = {{(i,j): 7*D[i,j] for i in range(m-1) for j in range(n-1)}}
 
 y = M.addVars(range(n), name="y") # y[0], ..., y[n-1]
                                     # name= "y" is optional. If omited, name is taken from "y ="
-
 M.setObjective(sum(c[j]*y[j] for j in range(n) ), GRB.MAXIMIZE)
-
 M.addConstrs((sum(A[i,j]*y[j] for j in range(n) ) <= b[i] for i in range(m)), "constr") # constraint must be in () if name argument used
-
 M.write('M.lp') # Write model in LP format
-
 M.optimize()
 
-for j in range(n):
-    print(y[j].varName, y[j].x)  # y is the variable name, ".x" references the optimal solution of the variable
-print('Obj: %g' % M.ObjVal)
-
-# Error handling (from matrix1.py)
-# except gp.GurobiError as e:
-#    print('Error code ' + str(e.errno) + ": " + str(e))
-
-# except AttributeError:
-#    print('Encountered an attribute error')
+if M.status == GRB.OPTIMAL:
+    for j in range(n):
+        print(y[j].varName, '=', y[j].x)  # y is the LP variable, ".varName" gives its name, ".x" eferencesr the gives its optimal value
+    print('Obj: %g' % M.ObjVal)
+else:
+    print('No solution')
 
 ### Set indexing: see diet.py, which also has some shortcut commands
 
@@ -66,8 +71,7 @@ J = ['Tester', 'JavaDeveloper', 'Architect'] # Jobs
 
 # Matching rewards. See gp.multidict for shortcuts entering double indices or multiple values for the same indices.
 C = {
-  ('Carlos','Tester'): 53, # C is a list of lists.
-  # Refer to it as C['Carlos','Tester']. But must refer to nested lists with numeric indices as c[0][0], which also works for arrays.
+  ('Carlos','Tester'): 53, # C is a dictionary. Refer to it as C[i,j], not C[i][j] or C['Carlos','Tester'] (strings not converted error)
   ('Carlos','JavaDeveloper'): 27,
   ('Carlos','Architect'): 13,
   ('Joe','Tester'): 80,
@@ -80,16 +84,28 @@ C = {
 x   = a.addVars(I, J, lb = 0,ub = 1, name='x') # 0 <= x[i,j] <= 1,  i in I, j in J. Continuous variables.
 # x = a.addVars(I, J, vtype = GRB.BINARY, name='x') # x[i,j] = 0,1, i in I, j in J. Binary variables.
 
-a.setObjective(sum(C[i,j]*x[i,j] for i in I for j in J), GRB.MAXIMIZE)
-
+a.setObjective(sum(C[i,j]*x[i,j] for i in I for j in J), GRB.MAXIMIZE) # Gurobi var's are arrays, refer to as x[i,j], not x[i][j] 
 a.addConstrs((x.sum('*',j) == 1 for j in J), name='job') # constraint must be in () if name argument used
 a.addConstrs((x.sum(i,'*') <= 1 for i in I), name='resource')
-
 a.write('assignment_problem.lp')
 a.optimize()
 
 for v in a.getVars(): # Get variable names, print only positive variables
     if v.x > 1e-6:
-        print(v.varName, v.x)
-#print('x[Carlos,Tester]:' % x['Carlos','Tester'].X)  # x is the variable name, ".X" references the optimal solution of the variable
+        print(v.varName, '=', v.x)
+
+print( '\nSame output using loops and x[i,j]')
+for i in I:
+    for j in J:
+        if x[i,j].X > 1e-6:
+            print('x[', i, ',', j, '] =', x[i,j].X)
+
+print( 'x[Carlos,Tester]:' , x["Carlos","Tester"].X)  # not working: string arguments not converted
 print('Obj: %g' % a.ObjVal)
+
+# Error handling from matrix1.py (not working)
+# except gp.GurobiError as e:
+#    print('Error code ' + str(e.errno) + ": " + str(e))
+
+# except AttributeError:
+#    print('Encountered an attribute error')
